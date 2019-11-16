68a622e952bef20559c75a0ed80341e75f059e3d
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2019-11-16 15:11:00 -0300

    add debt attribute to Client class

--- project/entity/client.py
+++ project/entity/client.py
@@ -5,6 +5,13 @@ class Client(object):
         self.name = name
         self.address = address
         self.phone = phone
+        self.debt = 0
 
     def __repr__(self):
         return f'Client{self.id_number, self.name, self.address, self.phone}'
+
+    def add_debt(self, value):
+        self.debt += value
+
+    def reset_debt(self):
+        self.debt = 0
