22a4888911661e9c7742375d6de363fda562f23c
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2019-11-17 19:00:26 -0300

    create ProductController class

---
+++ project/controler/product.py
@@ -0,0 +1,34 @@
+from project.entity.product import Product
+from project.model.model import db
+
+
+class ProductController(object):
+
+    @staticmethod
+    def insert_product(product: Product):
+        return db['product'].insert(product.__dict__)
+
+    @staticmethod
+    def update_product(id: int, product: Product):
+        return db['product'].update(id, product.__dict__)
+
+    @staticmethod
+    def delete_product(id):
+        return db['product'].delete(id)
+
+    @staticmethod
+    def select_product(id):
+        return db['product'].select(id)
+
+    @staticmethod
+    def list_products():
+        return db['product'].select()
+
+    @classmethod
+    def add_quantity_product(cls, id: int, value: int):
+        product = Product(**cls.select_product(id))
+        if product:
+            product.add_quantity(value)
+            cls.update_product(id, product)
+            return True
+        return False
